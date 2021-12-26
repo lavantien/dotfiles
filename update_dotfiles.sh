24474b0d3ec91178b6cc9e84e9befe4b0d36d524
lavantien <lavantien96@gmail.com>
2021-12-26 11:29:36 +0700

    Add zk config

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -1,1 +1,1 @@
-cd ~ && cp ~/.zshrc ~/.zshenv ~/.config/wezterm/wezterm.lua ~/dotfiles && cd ~/dotfiles
+cd ~ && cp ~/.zshrc ~/.zshenv ~/.config/wezterm/wezterm.lua ~/Documents/notebook/.zk/config.toml ~/dotfiles && cd ~/dotfiles
