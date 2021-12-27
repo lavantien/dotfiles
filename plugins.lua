97011d6857506d6135ac49027b1b6b80043d8c9e
lavantien <lavantien96@gmail.com>
2021-12-28 00:47:46 +0700

    Add Neovim config

---
+++ plugins.lua
@@ -0,0 +1,11 @@
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  vim.cmd([[
+    augroup packer_user_config
+      autocmd!
+      autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+    augroup end
+  ]])
+end)
+
+
--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -1,1 +1,1 @@
-cd ~ && cp ~/.zshrc ~/.zshenv ~/.config/wezterm/wezterm.lua ~/Documents/notebook/.zk/config.toml ~/dotfiles && cd ~/dotfiles
+cd ~ && cp ~/.zshrc ~/.zshenv ~/.config/wezterm/wezterm.lua ~/Documents/notebook/.zk/config.toml ~/.config/nvim/init.lua ~/.config/nvim/plugins.lua ~/dotfiles && cd ~/dotfiles
