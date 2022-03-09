b98095f5619fed275f9e8c1918708bf900fbfedd
lavantien <lavantien96@gmail.com>
2022-03-09 21:14:08 +0700

    Add deploy script to restore dotfiles in new system

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+cp .zshrc .zshenv ~/
+
+mkdir -p ~/.config/wezterm/colors
+
+mkdir -p ~/.config/nvim
+
+mkdir -p ~/.config/Code/User
+
+cp wezterm.lua ~/.config/wezterm/
+
+cp wezterm_tokyonight_night.toml wezterm_tokyonight_day.toml wezterm_github_dark_brighten.toml ~/.config/wezterm/colors/
+
+cp init.vim ~/.config/nvim/
+
+cp settings.json keybindings.json ~/.config/Code/User/
+
+echo "deploy done!"
+
