9e20d130ca71d64799788f219484c666199057e2
lavantien <lavantien96@gmail.com>
2024-12-23 21:22:43 +0700

    enh(nvim): always wrap

--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -11,7 +11,8 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
-vim.opt.wrap = false
+vim.opt.wrap = true
+vim.o.linebreak = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
