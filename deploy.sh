8dd11dce835156e3386c018163bf6ac9bf014b04
lavantien <lavantien96@gmail.com>
2025-03-02 03:51:38 +0700

    feat: ai - support gpt-researcher

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,4 @@ cp git-clone-all.sh ~/dev/
 cp wezterm.lua ~/.config/wezterm/
 cp .aider.model.settings.yml ~/
 cp .aider.conf.yml.example ~/.aider.conf.yml
+cp .env.gpt-researcher ~/dev/gpt-researcher/.env
